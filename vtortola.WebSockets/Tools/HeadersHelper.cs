0cbff325bcb66d6a5ba5f6f8134edf03ecccc972
Denis Zykov <deniszykov@gmail.com>
2017-12-28 23:19:19 +0300

    [Common] removed unused code from helpers

--- vtortola.WebSockets/Tools/HeadersHelper.cs
+++ vtortola.WebSockets/Tools/HeadersHelper.cs
@@ -1,4 +1,4 @@
-﻿/*
+/*
 	Copyright (c) 2017 Denis Zykov
 	License: https://opensource.org/licenses/MIT
 */
@@ -12,49 +12,6 @@ namespace vtortola.WebSockets.Tools
 {
     internal static class HeadersHelper
     {
-        public static IList<string> SplitAndTrimInList(string value, StringSplitOptions options = StringSplitOptions.None, char valuesSeparator = ';')
-        {
-            if (value == null)
-                return (IList<string>)Enumerable.Empty<string>();
-
-            var valuesCount = CountChars(value, valuesSeparator) + 1;
-
-            var list = new List<string>(valuesCount);
-            foreach (var v in SplitAndTrim(value, options, valuesSeparator))
-                list.Add(v);
-
-            return list;
-        }
-
-        public static IEnumerable<string> SplitAndTrim(string valueString, StringSplitOptions options = StringSplitOptions.None, char valuesSeparator = ';')
-        {
-            if (valueString == null)
-                yield break;
-
-            var startIndex = 0;
-            do
-            {
-                var nextValueIndex = valueString.IndexOf(valuesSeparator, startIndex);
-                if (nextValueIndex < 0)
-                    nextValueIndex = valueString.Length;
-
-                var valueStart = startIndex;
-                var valueLength = Math.Max(0, nextValueIndex - startIndex);
-                var value = string.Empty;
-
-                TrimInPlace(valueString, ref valueStart, ref valueLength);
-
-                if (valueLength > 0)
-                    value = valueString.Substring(valueStart, valueLength);
-
-                if (options != StringSplitOptions.RemoveEmptyEntries || !string.IsNullOrEmpty(value))
-                    yield return value;
-
-                startIndex = nextValueIndex + 1;
-                continue;
-            } while (startIndex < valueString.Length);
-        }
-
         public static IEnumerable<KeyValuePair<string, string>> SplitAndTrimKeyValue(string valueString, char valuesSeparator = ';', char nameValueSeparator = '=', StringSplitOptions options = StringSplitOptions.None)
         {
             if (valueString == null)
@@ -97,38 +54,7 @@ namespace vtortola.WebSockets.Tools
                 startIndex = nextValueIndex + 1;
             } while (startIndex < valueString.Length);
         }
-
-        public static string Combine(IEnumerable<KeyValuePair<string, string>> keyValuePairs, char valuesSeparator = ';', char nameValueSeparator = '=')
-        {
-            if (keyValuePairs == null) throw new ArgumentNullException(nameof(keyValuePairs));
-
-            var sb = new StringBuilder();
-
-            foreach (var kv in keyValuePairs)
-            {
-                sb.Append(kv.Key)
-                    .Append(nameValueSeparator)
-                    .Append(kv.Value)
-                    .Append(valuesSeparator);
-            }
-
-            if (sb.Length > 0) sb.Length--;
-
-            return sb.ToString();
-        }
-
-        private static int CountChars(string value, char charToFind)
-        {
-            var count = 0;
-            foreach (var ch in value)
-            {
-                if (ch == charToFind)
-                    count++;
-            }
-
-            return count;
-        }
-
+        
         public static void TrimInPlace(string value, ref int startIndex, ref int length)
         {
             if (value == null) throw new ArgumentNullException(nameof(value));
